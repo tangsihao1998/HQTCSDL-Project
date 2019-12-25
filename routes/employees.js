4a009e6b4c903331c79604a0b3896ae7c8544e67
Genesis <genesisrhapsodos98@gmail.com>
2019-12-25 17:57:13 +0700

    Updated api routes for backend

--- routes/employees.js
+++ routes/employees.js
@@ -66,7 +66,88 @@ router.post('/login', async (req,res) => {
 	return res;
 });
 // ---------------------------------------------------------------------------------------------
+// employees/tour/edit
+// body:
+//  id: ID cua chuyen xe can chinh sua (int)
+//  vehID: ID cua chiec xe (int)
+//  departPlace: ID cua noi di (int)
+//  destPlace: ID cua noi den (int)
+//  departDate: thoi gian di (smalldatetime)
+//  duration: thoi gian hanh trinh (smallint)
+//  numOfSeats: tong so ghe (int)
+//  ticketPrice: gia ve (money)
 
+router.post('/tour/edit', async (req,res) => {
+  const tour = req.body;
+  // them _resolved vao sau doan EXEC o duoi khi demo
+	const reqString = `EXEC dbo.sp_editActiveTour ${tour.id} ${tour.vehID} ${tour.departPlace} ${tour.destPlace} ${tour.departDate} ${tour.duration} ${tour.numOfSeats} ${tour.ticketPrice} `;
+	const request = new Request( reqString,
+    (err, rowCount) => {
+      if (err) {
+        console.error('ERR: ', err.message);
+      } else {
+				console.log(`${rowCount} row(s) returned`);
+      }
+		}
+	)
+  // Xu ly sau khi chay stored proc (copy y nhu o tren xuong, chua chinh sua)
+
+	// request.on("row", columns => {
+  //   columns.forEach(column => {
+	// 		if( column.value == true){
+	// 			console.log('Success');
+	// 			res.json({Success: "Success"});
+	// 			// Phân quyền 
+	// 			//tao lai connection = tk user
+	// 		}
+	// 		else{
+	// 			console.log('Fail')
+	// 			res.json({Success: "Fail"});
+	// 		}
+  //   });
+  // });
+
+	// connection.execSql(request);
+	// return res;
+});
+
+// ---------------------------------------------------------------------------------------------
+// employees/tour -> get all tours
+router.post('/tour', async (req,res) => {
+  const tour = req.body;
+  // them _resolved vao sau doan EXEC o duoi khi demo
+	const reqString = `EXEC dbo.sp_getAllTours`;
+	const request = new Request( reqString,
+    (err, rowCount) => {
+      if (err) {
+        console.error('ERR: ', err.message);
+      } else {
+				console.log(`${rowCount} row(s) returned`);
+      }
+		}
+  )
+  // Tra ve table chua tat ca cac tour, xem trong script khoi tao database de biet co nhung column nao
+  
+  // Xu ly sau khi chay stored proc (copy y nhu o tren xuong, chua chinh sua)
+  
+	// request.on("row", columns => {
+  //   columns.forEach(column => {
+	// 		if( column.value == true){
+	// 			console.log('Success');
+	// 			res.json({Success: "Success"});
+	// 			// Phân quyền 
+	// 			//tao lai connection = tk user
+	// 		}
+	// 		else{
+	// 			console.log('Fail')
+	// 			res.json({Success: "Fail"});
+	// 		}
+  //   });
+  // });
+
+	// connection.execSql(request);
+	// return res;
+});
 
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,127 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// ---------------------------------------------------------------------------------------------
+// users/tour -> get all tours
+router.post('/tour', async (req,res) => {
+  const tour = req.body;
+  // them _resolved vao sau doan EXEC o duoi khi demo
+	const reqString = `EXEC dbo.sp_getAllTours`;
+	const request = new Request( reqString,
+    (err, rowCount) => {
+      if (err) {
+        console.error('ERR: ', err.message);
+      } else {
+				console.log(`${rowCount} row(s) returned`);
+      }
+		}
+  )
+  // Tra ve table chua tat ca cac tour, xem trong script khoi tao database de biet co nhung column nao
+  
+  // Xu ly sau khi chay stored proc (copy y nhu o tren xuong, chua chinh sua)
+  
+	// request.on("row", columns => {
+  //   columns.forEach(column => {
+	// 		if( column.value == true){
+	// 			console.log('Success');
+	// 			res.json({Success: "Success"});
+	// 			// Phân quyền 
+	// 			//tao lai connection = tk user
+	// 		}
+	// 		else{
+	// 			console.log('Fail')
+	// 			res.json({Success: "Fail"});
+	// 		}
+  //   });
+  // });
+
+	// connection.execSql(request);
+	// return res;
+});
+
+// ---------------------------------------------------------------------------------------------
+// users/booking/add -> route de user dat ve xe
+// body:
+//  userID: truyen vao ID cua user dang dang nhap
+//  activeTourID: truyen vao ID cua chuyen xe ma user dat ve
+//  departPlace: noi khoi hanh cua chuyen xe
+//  departDate: thoi gian khoi hanh cua chuyen xe
+//  destPlace: noi den cua chuyen xe
+//  price: gia ve
+//  seatID: so ghe
+//  timePlaced: hoi Kha, t cung k biet cai nay
+
+router.post('/tour', async (req,res) => {
+  const booking = req.body;
+  // them _resolved vao sau doan EXEC o duoi khi demo
+	const reqString = `EXEC dbo.sp_createBooking ${booking.userID} ${booking.activeTourID} ${booking.departPlace} ${booking.departDate} ${booking.destPlace} ${booking.price} ${booking.seatID} ${booking.timePlaced}`;
+	const request = new Request( reqString,
+    (err, rowCount) => {
+      if (err) {
+        console.error('ERR: ', err.message);
+      } else {
+				console.log(`${rowCount} row(s) returned`);
+      }
+		}
+  )
+  // Xu ly sau khi chay stored proc (copy y nhu o tren xuong, chua chinh sua)
+  
+	// request.on("row", columns => {
+  //   columns.forEach(column => {
+	// 		if( column.value == true){
+	// 			console.log('Success');
+	// 			res.json({Success: "Success"});
+	// 			// Phân quyền 
+	// 			//tao lai connection = tk user
+	// 		}
+	// 		else{
+	// 			console.log('Fail')
+	// 			res.json({Success: "Fail"});
+	// 		}
+  //   });
+  // });
+
+	// connection.execSql(request);
+	// return res;
+});
+
+// ---------------------------------------------------------------------------------------------
+// users/unavailable_seats?id=<id chuyen xe>
+
+router.post('/unavailable_seats', async (req,res) => {
+  const activeTourID = req.query.id;
+  // them _resolved vao sau doan EXEC o duoi khi demo
+	const reqString = `EXEC dbo.sp_getUnavailableSeats ${activeTourID}`;
+	const request = new Request( reqString,
+    (err, rowCount) => {
+      if (err) {
+        console.error('ERR: ', err.message);
+      } else {
+				console.log(`${rowCount} row(s) returned`);
+      }
+		}
+  )
+  // Sau khi chay xong, tra ve tat ca ma so ghe da bi nguoi khac dat
+
+  // Xu ly sau khi chay stored proc (copy y nhu o tren xuong, chua chinh sua)
+  
+	// request.on("row", columns => {
+  //   columns.forEach(column => {
+	// 		if( column.value == true){
+	// 			console.log('Success');
+	// 			res.json({Success: "Success"});
+	// 			// Phân quyền 
+	// 			//tao lai connection = tk user
+	// 		}
+	// 		else{
+	// 			console.log('Fail')
+	// 			res.json({Success: "Fail"});
+	// 		}
+  //   });
+  // });
+
+	// connection.execSql(request);
+	// return res;
+});
+
 module.exports = router;
