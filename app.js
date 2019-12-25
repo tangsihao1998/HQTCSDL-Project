05e9242e0a87edbe83df7d9c0aebcaca6927f62f
Si Hao <tangsihao1998@gmail.com>
2019-12-25 13:55:07 +0700

    Test

--- app.js
+++ app.js
@@ -54,6 +54,42 @@ connection.on("connect", err => {
   }
 });
 
+// Attempt to connect and execute queries if connection goes through
+connection.on("connect", err => {
+  if (err) {
+    console.error(err.message);
+  } else {
+    queryDatabase();
+  }
+});
+
+function queryDatabase() {
+  console.log("Reading rows from the Table...");
+
+  // Read all rows from table
+  const request = new Request(
+    `SELECT TOP 20 pc.Name as CategoryName,
+                   p.name as ProductName
+     FROM [SalesLT].[ProductCategory] pc
+     JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`,
+    (err, rowCount) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log(`${rowCount} row(s) returned`);
+      }
+    }
+  );
+
+  request.on("row", columns => {
+    columns.forEach(column => {
+      console.log("%s\t%s", column.metadata.colName, column.value);
+    });
+  });
+
+  connection.execSql(request);
+}
+
 // ---------------------------------------------------------------------------------------------
 
 app.use('/', indexRouter);
